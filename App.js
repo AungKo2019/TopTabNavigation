58bc25417cb7956fc465f3a9a13dbab7ed342797
AungKo2019 <highflier.ako1991@gmail.com>
2023-05-11 22:50:06 +0630

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import ChatScreen from './screens/Chat'
+import ContactScreen from './screens/Contact';
+import HomeScreen from './screens/Home';
+
+const Tab=createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,marginTop:30,backgroundColor:'red'}}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name='Home' component={HomeScreen}/>
+          <Tab.Screen name='Chat' component={ChatScreen}/>
+          <Tab.Screen name='Contact' component={ContactScreen}/>
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
+  
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ screens/Contact.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <View style={{flex:1,alignItems:'center',justifyContent:'center',backgroundColor:'yellow'}}>
+      <Text>This is Contact Screen</Text>
+    </View>
+  )
+}
+
+export default Contact
